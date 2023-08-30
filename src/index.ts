b699389bc83945a34245abc62d02919f329bdc8b
Shimmy Dasari <shimmyroy.dasari@IN-MB-50204.local>
2023-08-30 09:32:28 +0530

    Combine start commands

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import fetchApiData from "./api/users";
 import { User } from "./types/users";
 
 const main = async () => {
-    const jsonData: Array<User> = await fetchApiData();
-    console.log('Fetched JSON data: \n', jsonData);
+    const jsonData: Array<User> | Error = await fetchApiData();
+    if (jsonData instanceof Error) {
+        console.error(jsonData.message);
+    } else {
+        console.log('Fetched JSON data: \n', jsonData);
+    }
 }
 main();
 
